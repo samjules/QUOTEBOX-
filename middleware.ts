78ee48773849b306e1040963dbb302528cbe4222
samjules <samjulander@gmail.com>
2026-02-20 21:21:15 -0900

    .

--- middleware.ts
+++ middleware.ts
@@ -58,6 +58,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|css|js|map|woff|woff2|ttf|eot|ico)$).*)',
   ],
 }
